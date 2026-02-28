359879e8b6d112df94665bcd20649dd66cb4d067
bluewhale0 <lideng1210@gmail.com>
2026-03-01 03:23:18 +0800

    rustfmt changes

--- crates/clawshake-broker/src/main.rs
+++ crates/clawshake-broker/src/main.rs
@@ -52,8 +52,11 @@ enum Command {
 async fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "clawshake_broker=info,warn".parse().expect("valid tracing filter")),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                "clawshake_broker=info,warn"
+                    .parse()
+                    .expect("valid tracing filter")
+            }),
         )
         // MCP stdio: log to stderr so we don't pollute the JSON-RPC channel.
         .with_writer(std::io::stderr)
